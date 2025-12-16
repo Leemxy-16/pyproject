7f84486a526716741992c202af20e3c00c892100
leemxy-16 <herleemahaliyu@gmail.com>
2025-12-16 13:20:57 +0100

    Implement budget tracking and expense reporting functionality

--- expense.py
+++ expense.py
@@ -1,1 +1,26 @@
-budget = int()
+budget = int(input("Enter your total budget: "))
+expense = {}
+total = 0
+answer = ' new'
+
+while answer != 'stop':
+    item = input("Enter the expense item: ")
+    cost = float(input(f"Enter the cost for {item}: "))
+    expense[item] = cost
+    total += cost
+    answer = input(
+        "Type 'stop' to finish or press Enter to add another expense: ").lower()
+
+for i in expense.items():
+    print(f"The cost of {i[0]} is {i[1]}")
+print(f"Your total expenses amount to: {total}")
+
+if (budget < total):
+    print(
+        f"You have exceeded your ${budget}. Your total expenses are ${total}.")
+elif (budget > total):
+    print(
+        f"You are within your budget of ${budget}. Your total expenses are ${total}.")
+else:
+    print(
+        f"You have exactly met your budget of ${budget}. Your total expenses are ${total}.")
